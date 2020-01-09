9a667a4b82cf3cde508daf217731ee92d10b588c
username <qkrdydals327@gmail.com>
2020-01-09 16:02:16 +0900

    Updates

---
+++ precache-manifest.320cbf464e593e1cb17fb92cdc77a80e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aed3283e1d11caff454e4fd13326330",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "9a73b3634107902c74dc",
+    "url": "/movie_app_2019/static/css/main.3c10ec96.chunk.css"
+  },
+  {
+    "revision": "a72187a7447ced0137d3",
+    "url": "/movie_app_2019/static/js/2.ab7bf560.chunk.js"
+  },
+  {
+    "revision": "9a73b3634107902c74dc",
+    "url": "/movie_app_2019/static/js/main.4078a2e9.chunk.js"
+  },
+  {
+    "revision": "5a2b9365f7ca01d151d6",
+    "url": "/movie_app_2019/static/js/runtime~main.64ac130e.js"
+  }
+]);
